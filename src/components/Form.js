fe29e9d3c066f3e04cf4f2fd86f039f6e9b6ea44
Luciano Armoa <mmannuarmoa@gmail.com>
2021-12-27 07:46:52 -0400

    comentarios en codigo

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,5 @@
 import React, {useRef} from "react";
-import { connect } from "react-redux";
+import { connect, useSelector } from "react-redux";
 import {guardarText, alertInit} from "./redux/actions/action";
 
 const Form = ({guardarText, alertInit}) => {
@@ -11,6 +11,11 @@ const Form = ({guardarText, alertInit}) => {
         guardarText(textRef.current.value);
         alertInit();
     }
+
+    //--------------------------se utiliza para obtener el estado del reducer y mostrar el texto con ternario si es true o no
+    const alert = useSelector(state => state.reducerGlobal.showAlert);
+
+    console.log('Form: ', alert);
     
     
 
@@ -18,7 +23,7 @@ const Form = ({guardarText, alertInit}) => {
         <div>
             <input type="text" ref={textRef}/>
             <button onClick={() => {guardarText(textRef.current.value)}} >Guardar</button>
-            <button onClick={() => {alertInit()}} >Alert-ocultar</button>
+            <button onClick={() => {alertInit()}} >{alert ? 'Ocultar': 'Mostrar'}</button>
             <button onClick={handleSubmit} >Guardar y mostrar boton</button>
         </div>
     )
